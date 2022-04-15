8431bc41b360d3257cd7dacb40898cba9164f472
Sorin Badea <sorinbadea29@gmail.com>
2022-04-15 12:00:19 +0300

    final commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filteredChangeHandler = (selectedYear) => {
-    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
@@ -17,19 +17,19 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-
   return (
     <div>
       <li>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filteredChangeHandler}
-        />
+        <Card className="expenses">
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filteredChangeHandler}
+          />
 
-        <ExpensesList items={filteredExpenses}/>
+          <ExpensesChart expenses={filteredExpenses}/>
 
-      </Card>
+          <ExpensesList items={filteredExpenses} />
+        </Card>
       </li>
     </div>
   );
